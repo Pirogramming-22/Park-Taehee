1c4375f69423ba07ce8ea4154c75fd1db68abcd7
qkrxogmla <suk05553@naver.com>
2025-01-03 23:13:24 +0900

    7단계

--- Park-Taehee/python_problem/python_problem_1.py
+++ Park-Taehee/python_problem/python_problem_1.py
@@ -16,6 +16,7 @@ while(True):
         i += 1
         print("playerA: ", format(i))
         if i==31:
+            print("playerB win!")
             exit()
     while (True):
         num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
@@ -32,4 +33,5 @@ while(True):
         i += 1
         print("playerB: ", format(i))
         if i==31:
+            print("playerA win!")
             exit()
