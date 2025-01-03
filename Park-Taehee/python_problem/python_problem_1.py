09e1c43b6540db59f655eef5579e91e02e5ae3f4
qkrxogmla <suk05553@naver.com>
2025-01-03 22:27:47 +0900

    4단계

--- Park-Taehee/python_problem/python_problem_1.py
+++ Park-Taehee/python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num=0
+i=0
 while(True):
     num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
     if num.isdigit():
@@ -9,3 +10,6 @@ while(True):
             print("1, 2, 3 중 하나를 입력하세요")
     else:
         print("정수를 입력하세요")
+for i in range(num):
+    i+=1
+    print("player: ",format(i))
