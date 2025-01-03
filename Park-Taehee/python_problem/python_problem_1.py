06d607000d22c0df5f55cb6d4432bdaba785b2df
qkrxogmla <suk05553@naver.com>
2025-01-03 23:11:03 +0900

    6단계

--- Park-Taehee/python_problem/python_problem_1.py
+++ Park-Taehee/python_problem/python_problem_1.py
@@ -1,30 +1,37 @@
 num=0
 i=0
 while(True):
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    if num.isdigit():
-        num=int(num)
-        if 1<=num<=3:
-            break
+    while (True):
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        if num.isdigit():
+            num = int(num)
+            if 1 <= num <= 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
         else:
-            print("1, 2, 3 중 하나를 입력하세요")
-    else:
-        print("정수를 입력하세요")
-out=0
-for out in range(num):
-    i+=1
-    print("playerA: ",format(i))
-while(True):
-    num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    if num.isdigit():
-        num=int(num)
-        if 1<=num<=3:
-            break
+            print("정수를 입력하세요")
+    out = 0
+    for out in range(num):
+        i += 1
+        print("playerA: ", format(i))
+        if i==31:
+            print("playerB win!")
+            exit()
+    while (True):
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        if num.isdigit():
+            num = int(num)
+            if 1 <= num <= 3:
+                break
+            else:
+                print("1, 2, 3 중 하나를 입력하세요")
         else:
-            print("1, 2, 3 중 하나를 입력하세요")
-    else:
-        print("정수를 입력하세요")
-out=0
-for out in range(num):
-    i += 1
-    print("playerB: ",format(i))
+            print("정수를 입력하세요")
+    out = 0
+    for out in range(num):
+        i += 1
+        print("playerB: ", format(i))
+        if i==31:
+            print("playerA win!")
+            exit()
