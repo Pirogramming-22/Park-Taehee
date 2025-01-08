82e6be56a463a357e346a33933c5e69af22aed14
qkrxogmla <suk05553@naver.com>
2025-01-08 12:36:34 +0900

    Django 모델

--- Park-Taehee/djangogirls/mysite/settings.py
+++ Park-Taehee/djangogirls/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} 
 
 
 # Password validation
